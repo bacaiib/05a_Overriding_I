ab5c0928ac17c0244fdd32ccd71039750cc2f808
bacaiib <roman-frommhold@gmx.net>
2024-01-30 11:47:27 +0100

    Child / Parent

---
+++ com/cc/java/Child.java
@@ -0,0 +1,10 @@
+package com.cc.java;
+
+public class Child extends Parent{
+
+    public String sayHello(){
+        return "hello from Child";
+    }
+
+
+}
---
+++ com/cc/java/Parent.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Parent {
+
+    public String sayHello(){
+        return "hello from perent";
+    }
+    
+    
+
+}
